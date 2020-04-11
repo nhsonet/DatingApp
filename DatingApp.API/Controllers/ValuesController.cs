ba88f9bfc992809d5da4820c752c56361c3488c5
nhsonet <nh.sonet@outlook.com>
2020-04-11 20:54:49 +0600

    End of Section 4

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -22,7 +22,7 @@ namespace DatingApp.API.Controllers
 
         }
 
-        // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
@@ -40,19 +40,16 @@ namespace DatingApp.API.Controllers
             return Ok(value);
         }
 
-        // POST api/values
         [HttpPost]
         public void Post([FromBody] string value)
         {
         }
 
-        // PUT api/values/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
         }
-
-        // DELETE api/values/5
+        
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
